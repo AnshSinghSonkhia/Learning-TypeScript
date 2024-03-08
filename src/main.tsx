9de22e559dd90a097729622a0847b4d6fa005c54
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2024-03-09 01:09:27 +0530

    Solved the draggable issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-// also known as "index.tsx"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
